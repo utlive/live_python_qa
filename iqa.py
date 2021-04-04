3b850068ec1f48e749b06318e65e1a13629dceef
JoshuaEbenezer <joshuapeterebenezer@gmail.com>
2021-04-04 17:29:30 -0500

    correct Brisque

--- iqa.py
+++ iqa.py
@@ -2,9 +2,11 @@ from .utils import compute_image_mscn_transform,extract_subband_feats
 import cv2
 import numpy as np
 
+# TODO: Compare with Matlab
 def brisque(image):
     y_mscn = compute_image_mscn_transform(image)
     half_scale = cv2.resize(image, dsize=(0,0),fx=0.5,fy=0.5, interpolation=cv2.INTER_LANCZOS4)
+    y_half_mscn =compute_image_mscn_transform(half_scale) 
     feats_full = extract_subband_feats(y_mscn)
-    feats_half = extract_subband_feats(half_scale)
+    feats_half = extract_subband_feats(y_half_mscn)
     return np.concatenate((feats_full,feats_half))
